db01d87918bc7f7305699212b2cf4fb4bc0e5a97
Srikalyan Megati <kalmegati@gmail.com>
2019-09-26 22:50:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,16 @@
 # Add  code here!
 
 def prime?(number)
+  factor = 1
   counter = 2
   if number < 2
     return false
   else
-    while counter < number
+    while counter < number/factor
       if number % counter == 0
         return false
       end
+      factor = counter
       counter += 1
     end
   end
